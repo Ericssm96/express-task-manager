fd661a7d6ac18539d0d8ed5d6d8ca50267bf6404
Eric Stefano Silva Milhomem <94946025+Ericssm96@users.noreply.github.com>
2022-09-15 21:05:15 -0300

    Implementou modelo MVC com as rotas das tasks

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 5000;
+const tasks = require('./routes/tasks');
+
+// middleware
+app.use(express.json());
+
+// routes
+app.use('/api/v1/tasks', tasks);
+
+app.get('/hello', (req, res)=>{
+    res.send("Task manager app")
+})
+
+app.listen(port, ()=>{
+    console.log(`Server listening on port ${port}.`)
+})
