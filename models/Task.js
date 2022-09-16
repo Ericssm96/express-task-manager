b8e47b24e6955ac8dbbf14a0034ead84994ded20
Eric Stefano Silva Milhomem <94946025+Ericssm96@users.noreply.github.com>
2022-09-16 09:57:09 -0300

    Definiu validação para entrada de dados no banco

--- models/Task.js
+++ models/Task.js
@@ -1,10 +1,22 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema(
-    {
-        name: String,
-        completed: Boolean
+  {
+    name: {
+      type: String,
+      required: [true, "Must provide a task name."], // I could just set this to true, but
+                                                    // since I can edit the error message
+                                                    // too, I'll do it
+      trim: true,
+      maxLength: [30, "Name can't have more than 30 characters."]
+    },
+    completed: {
+      type: Boolean,
+      default: false
     }
+  }
 ); // defining the schema structure
 
-module.exports = mongoose.model('Task', TaskSchema); // Here I define the name of the document this schema should create
+module.exports = mongoose.model('Task', TaskSchema); // Here I define the name of the
+                                                     // document this schema should
+                                                     // create
